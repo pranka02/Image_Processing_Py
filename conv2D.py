48916afaecaa020def5e1d00bc68479a5cdcf073
Priyanka Mohandas <priyankamohandas02@gmail.com>
2017-02-23 13:56:11 -0500

    Add files via upload

---
+++ conv2D.py
@@ -0,0 +1,83 @@
+# This program implements a 2D convolution function.
+
+import numpy as np
+import cv2
+
+
+def conv2D(inp,ker):
+	ir = len(inp)												# no of rows in input 
+	ic = len(inp[0])											# no of columns in input 
+
+
+	kr = len(ker)												# no of rows in kernel
+	kc = len(ker[0])											# no of columns in kernel
+
+	m = ir + kr -1												# no of rows in output
+	n = ic + kc -1												# no of columns in output 
+
+	y = np.zeros((m,n))
+
+	kcentx =round(kr/2- 0.5)									# x coordinate of center of kernel 
+	kcenty =round(kc/2- 0.5)									# y coordinate of center of kernel 
+
+	padrow = round(kr/2)
+	padcol = round(kr/2)
+	inp = np.lib.pad(inp,(padrow,padcol),'constant',constant_values= 0)	# zero-padding input 
+
+	inr = len(inp)												# no of rows in modified input
+	inc = len(inp[0])											# no of columns in modified input
+
+	length = kr -kcenty											# length from center of kernel
+	width = kc -kcentx											# width from center of kernel
+	
+
+	rowmin = length
+	rowmax = length
+	colmin = width
+	colmax = width
+
+	ker = np.flip(ker, axis=0)									# flipping kernel horizontally and vertically
+	ker = np.fliplr(ker)
+
+	for k in range(m):											# convolution starts
+			
+		for l in range(n):
+			
+			y[k][l] = 0
+			s = k-rowmin
+			t = k+rowmax
+			u = l-colmin
+			v = l+colmax
+
+			if s<0:
+				s=max(0,s)
+			elif s>=0:
+				s +=1
+
+
+			if t>m:
+				t=min(m,t)
+
+			if u<0:
+				u=max(0,u)
+			elif u>=0:
+				 u+=1
+
+			if v>m:
+				v=min(n,v)
+
+
+			for i in range(s,t):
+				
+				for j in range(u,v):
+					p = i-k+kcentx
+					q = j-l+kcenty
+					y[k][l] += inp[i][j]*ker[p][q]
+
+	return y
+		
+	
+
+
+
+
---
+++ filterimg.py
@@ -0,0 +1,57 @@
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+from PIL import Image
+from numpy import array
+from conv2D import conv2D
+
+def filtimg(imagepath,filt):
+	img = Image.open(imagepath).convert('L')					# open image and convert to grayscale
+	inp = array(img)
+	lenimg= len(inp)
+	widimg = len(inp[0])
+	size = lenimg*widimg										# convert to numoy array
+	x = inp.dtype
+	img.show()													# display image 
+							
+	y = conv2D(inp,filt)								
+	siz1= len(y)
+
+
+	for i in range(siz1):										# normalizing pixel values  
+		for j in range(siz1):
+			if y[i][j] >255:
+				y[i][j] =255
+			if y[i][j] <0:
+				y[i][j] =0
+
+	y = y.astype(np.uint8)												# converting to unit8
+
+	fftimg = np.fft.fft2(inp)
+	fftimgs =np.fft.fftshift(fftimg)									# calculating fft of input image 
+	fftfiltimg = np.fft.fft2(y)
+	fftfiltimgs =np.fft.fftshift(fftfiltimg)							# calculating fft of filtered image 
+	fftfilt = np.fft.fft2(filt, s=(lenimg, widimg))				
+	fftfilts = np.fft.fftshift(fftfilt)
+
+	fig, ax = plt.subplots(nrows=1, ncols=3, figsize=(20, 20))
+	ax[0].imshow(abs(np.log(fftimgs)), interpolation="none", origin='lower')
+	ax[0].set_title('log magnitude of input image')
+	ax[1].imshow(abs(np.log(fftfiltimgs)), interpolation="none", origin='lower')
+	ax[1].set_title('log magnitude of filtered image')
+	ax[2].imshow(abs((fftfilts)), interpolation="none", origin='lower')
+	# ax[2].imshow(fftfilt.ravel(), bins=100) take teh magnitude of the fft before histogram
+	ax[2].set_title('frequency response of filter')
+	plt.savefig('plot.png')
+	plt.show()
+
+
+
+	filtimg= Image.fromarray(y, 'L')
+	filtimg.show()
+	filtimg.save('filtimage.png')
+	
+	return 
+
+
+
